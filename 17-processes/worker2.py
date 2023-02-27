26968c6833b52dbbdc99a3176b4f39f9bbb70780
John Hunt <john.hunt10@gmail.com>
2023-02-27 14:39:03 +0000

    Initial import

---
+++ 17-processes/worker2.py
@@ -0,0 +1,21 @@
+from multiprocessing import Process, set_start_method
+from time import sleep
+import os
+
+
+def worker(msg):
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+    for i in range(0, 10):
+        print(msg, end='', flush=True)
+        sleep(1)
+
+
+if __name__ == '__main__':
+    print('Starting')
+    print('Root application process id:', os.getpid())
+    set_start_method('spawn')
+    t = Process(target=worker, args='A')
+    t.start()
+    print('Done')
