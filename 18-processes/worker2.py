619edc137761572a4b244db3c07ceb3bdba3b707
John Hunt <john.hunt10@gmail.com>
2023-03-08 17:11:06 +0000

    renaming

--- 18-processes/worker2.py
+++ 18-processes/worker2.py
@@ -1,21 +1,22 @@
 from multiprocessing import Process, set_start_method
 from time import sleep
-import os
-
 
 def worker(msg):
-    print('module name:', __name__)
-    print('parent process:', os.getppid())
-    print('process id:', os.getpid())
     for i in range(0, 10):
         print(msg, end='', flush=True)
         sleep(1)
 
 
 if __name__ == '__main__':
-    print('Starting')
-    print('Root application process id:', os.getpid())
     set_start_method('spawn')
-    t = Process(target=worker, args='A')
-    t.start()
+    print('Starting')
+
+    t2 = Process(target=worker, args='A')
+    t3 = Process(target=worker, args='B')
+    t4 = Process(target=worker, args='C')
+
+    t2.start()
+    t3.start()
+    t4.start()
+
     print('Done')
