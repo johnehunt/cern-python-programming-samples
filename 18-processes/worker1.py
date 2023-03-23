c96a56a7dbdc240d4cfdc90589850979598f38c6
John Hunt <john.hunt10@gmail.com>
2023-03-23 17:16:41 +0000

    tidying

--- 18-processes/worker1.py
+++ 18-processes/worker1.py
@@ -7,7 +7,7 @@ def worker(msg):
     print('module name:', __name__)
     print('parent process:', os.getppid())
     print('process id:', os.getpid())
-    for i in range(0, 10):
+    for _ in range(0, 10):
         print(msg, end='', flush=True)
         sleep(1)
 
--- 18-processes/worker2.py
+++ 18-processes/worker2.py
@@ -1,8 +1,11 @@
 from multiprocessing import Process, set_start_method
 from time import sleep
+import os
+
 
 def worker(msg):
-    for i in range(0, 10):
+    print(f'process id: {os.getpid()}')
+    for _ in range(0, 10):
         print(msg, end='', flush=True)
         sleep(1)
 
@@ -10,6 +13,7 @@ def worker(msg):
 if __name__ == '__main__':
     set_start_method('spawn')
     print('Starting')
+    print(f'Root application process id: {os.getpid()}')
 
     t2 = Process(target=worker, args='A')
     t3 = Process(target=worker, args='B')
