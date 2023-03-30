117ea3de67366ed193bbcb90d05c8d6e03acf715
John Hunt <john.hunt10@gmail.com>
2023-03-30 17:10:52 +0100

    Updated

--- 17-threads/simple_worker.py
+++ 17-threads/simple_worker.py
@@ -5,10 +5,9 @@ def simple_worker():
     print('hello')
 
 
-if __name__ == '__main__':
-    t1 = Thread(target=simple_worker)
-    t1.start()
+t1 = Thread(target=simple_worker)
+t1.start()
 
-    print(t1.name)
-    print(t1.ident)
-    print(t1.is_alive())
+print(t1.name)
+print(t1.ident)
+print(t1.is_alive())
--- 17-threads/timer_example.py
+++ 17-threads/timer_example.py
@@ -4,9 +4,9 @@ from threading import Timer
 def hello():
     print('hello')
 
-if __name__ == '__main__':
-    print('Starting')
-    t = Timer(5, hello)
-    t.start()
 
-    print('Done')
+print('Starting')
+t = Timer(5, hello)
+t.start()
+
+print('Done')
